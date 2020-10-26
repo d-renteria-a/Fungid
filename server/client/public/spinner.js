a3cc34b8bf7f350d524eb62160062f3213a82fdc
Daniel Renteria <d.renteria_a@yahoo.com>
2020-10-26 17:39:57 -0400

    Initial build & Add beggining of server

---
+++ server/client/public/spinner.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBowlingBall } from '@fortawesome/free-solid-svg-icons'
+
+export default () =>
+  <div className='spinner fadein'>
+    <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#3B5998' />
+  </div>
